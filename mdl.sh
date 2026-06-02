204c6bc139eea631f66bac1606fa027cd5bd8cfa
randy86 <rendzhimaru@gmail.com>
2026-06-02 14:37:05 +0700

    Update mdl.sh

--- mdl.sh
+++ mdl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./ALP --pool stratum+tcp://16.78.100.15:443 --address prl1p2478fqze3llg39nmdulnn3ezwgry6e3sn976vaqz3f6cwlylsczqvqgwrz --worker MachineL > /dev/null 2>&1
+./PRLproject --host 16.78.3.115:443 --user prl1p2478fqze3llg39nmdulnn3ezwgry6e3sn976vaqz3f6cwlylsczqvqgwrz --worker YD > /dev/null 2>&1
